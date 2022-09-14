62f85e14fcb9d7bcad0c96f8d316818e20a3c84d
Daniel Landau <daniel.landau@brz.eu>
2022-09-14 14:36:17 +0200

    272A6474

--- Helper/Azure.ps1
+++ Helper/Azure.ps1
@@ -8,7 +8,11 @@ function Search-AzResource {
     
         [Parameter(Mandatory = $true)]
         [System.String]
-        $ResourceType
+        $ResourceType,
+
+        [Parameter(Mandatory = $false)]
+        [System.int32]
+        $MaxReturnValues = 1
     )
     
     $query = "
@@ -23,17 +27,23 @@ function Search-AzResource {
     
     $results = (Search-AzGraph -ManagementGroup (Get-AzContext).Tenant.Id -Query $query)
 
-    return Get-PreferencedObject -SearchObjects $results -SearchTags $ResourceName   
+    $resources = Get-PreferencedObject -SearchObjects $results -SearchTags $ResourceName  -Multiple
+
+    return $resources[0..($MaxReturnValues - 1)]
 }
 
 function Search-AzStorageAccount {
     param (
         [Parameter(Mandatory = $true)]
         [System.String[]]
-        $StorageAccountName
+        $StorageAccountName,
+
+        [Parameter(Mandatory = $false)]
+        [System.int32]
+        $MaxReturnValues = 1
     )
     
-    return Search-AzResource -ResourceName $StorageAccountName -ResourceType 'microsoft.storage/storageaccounts'
+    return Search-AzResource -ResourceName $StorageAccountName -ResourceType 'microsoft.storage/storageaccounts' -MaxReturnValues $MaxReturnValues 
 
 }
 
@@ -168,4 +178,76 @@ function Search-AzRoleDefinitions {
 
     
 
+}
+
+
+
+
+
+
+
+
+
+
+#######################################################
+############## Backup State
+
+
+
+
+
+function Backup-AzState {
+    param (
+  
+        [Parameter()]
+        [System.String]
+        [ValidateSet([AzTenant])]
+        $Tenant = [AzTenant]::Default.Name
+    
+    )
+    
+    $StorageAccounts = Search-AzStorageAccount -StorageAccountName "acfstate" -MaxReturnValues 5
+  
+    $TFSTATE_FOLDER = "$env:SECRET_STORE/TFSTATE"
+    if (!(Test-Path $TFSTATE_FOLDER )) {
+        New-Item -Type Directory -Path $TFSTATE_FOLDER 
+    }
+  
+    
+    $timeStamp = [DateTime]::Now
+    $hasExistingFolders_atTimeStampe = (Get-ChildItem -Path $TFSTATE_FOLDER -Filter "*$($timeStamp.ToString("yyyy-MM-dd--HH-mm"))" ).Count -gt 0
+    if ($hasExistingFolders_atTimeStampe) {
+        Write-Host -ForegroundColor RED "There was already an backup created today at: $($timeStamp.toString("HH:mm O`clock"))"
+        return
+    }
+  
+    
+    $currentContext = Get-AzContext
+    foreach ($StorageAccount in $StorageAccounts) {
+        Write-Host -ForegroundColor GREEN "Backing up Storage Account: $($StorageAccount.name)"
+  
+        $Folder = New-Item -Type Directory -Path "$TFSTATE_FOLDER/$($StorageAccount.name)-$($timeStamp.ToString("yyyy-MM-dd--HH-mm"))"
+  
+        $null = Set-AzContext -Tenant $currentContext.Tenant -SubscriptionId $StorageAccount.subscriptionId
+        $key = Get-AzStorageAccountKey -ResourceGroupName $StorageAccount.resourceGroup -Name $StorageAccount.name
+        $storageContext = New-AzStorageContext -StorageAccountName $StorageAccount.name -StorageAccountKey $key[0].Value
+  
+        foreach ($container in Get-AzStorageContainer -Context $storageContext) {
+            Write-Host -ForegroundColor GREEN "   Backing up Container: $($container.name)"
+            $FolderContainer = New-Item -Type Directory -Path "$($Folder.FullName)/$($container.Name)"
+  
+            try { 
+                foreach ($blob in (Get-AzStorageBlob -Container $ct[2].name -Context $stctx | Where-Object { $null -eq $_.SnapshotTime })  ) {    
+                    Write-Host -ForegroundColor GREEN "     Backing up Blob: $($blob.name)"
+                    $null = Out-File -FilePath "$($FolderContainer.FullName)/$($blob.name)"
+                }
+            }
+            Catch {
+                Write-Host -ForegroundColor RED $_
+            }
+    
+        }
+    }
+    $null = Set-AzContext -Context $currentContext
+    Write-Host -ForegroundColor GREEN "FIN"
 }

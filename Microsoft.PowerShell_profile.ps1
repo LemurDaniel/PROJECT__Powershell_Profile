c2377688d292cb7ead69299df08097aefd0b7c88
LemurDaniel <landau.daniel.1998@gmail.com>
2023-01-11 22:20:16 +0100

    A81E099E

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -103,7 +103,6 @@ function Get-DumbJoke {
 . $env:PROFILE_HELPERS_PATH/Terraform.ps1
 . $env:PROFILE_HELPERS_PATH/DevOps.ps1
 . $env:PROFILE_HELPERS_PATH/Azure.ps1
-. $env:PROFILE_HELPERS_PATH/Prompt.ps1
  
 
 ## Initial Script

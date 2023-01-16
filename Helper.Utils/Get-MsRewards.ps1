b3df96ba7ba712328b903167300d93a9e72e6b71
LemurDaniel <landau.daniel.1998@gmail.com>
2023-01-17 00:57:07 +0100

    3C4746C8

--- Helper.Utils/Get-MsRewards.ps1
+++ Helper.Utils/Get-MsRewards.ps1
@@ -12,9 +12,10 @@ function Get-MsRewards {
 
     $words = Get-Content -Path "$PSScriptRoot\41.284_words.txt"
 
+    # Default Installation paths for browsers on windows.
     $applicationPaths = @{
         Edge   = 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
-        Opera  = 'C:\Users\Daniel\AppData\Local\Programs\Opera GX\launcher.exe'
+        Opera  = "C:\Users\$env:USERNAME\AppData\Local\Programs\Opera GX\launcher.exe"
         Chrome = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
     }
     $baseUrl = 'https://www.bing.com/search?q={0}'
@@ -22,12 +23,10 @@ function Get-MsRewards {
     for (; $calls -gt 0; $calls--) {
 
         Start-Sleep -Milliseconds (Get-Random -Minimum 800 -Maximum 2000)
-        # $word = Invoke-RestMethod -Method GET -Uri 'https://random-word-api.herokuapp.com/word'
-       
         $word = $words[(Get-Random -Minimum 0 -Maximum $words.Length)]
-
         $url = [System.String]::Format($baseUrl, $word)
         [system.Diagnostics.Process]::Start($applicationPaths[$browser], $url)
+        
     }
 
 }

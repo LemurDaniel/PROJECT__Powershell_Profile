478ea5d03b8f0ce0eb6961eda32f8df10510a98d
LemurDaniel <landau.daniel.1998@gmail.com>
2023-01-19 11:31:17 +0100

    D9723018

--- Helper.Utils/Get-Property.ps1
+++ Helper.Utils/Get-Property.ps1
@@ -1,4 +1,6 @@
 function Get-Property {
+
+    [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
         [System.Object]
@@ -10,11 +12,41 @@ function Get-Property {
         $PropertyPath
     )
 
-    if([System.String]::isNullOrEmpty($PropertyPath)){
+    if ([System.String]::isNullOrEmpty($PropertyPath)) {
         return $Object
     }
 
-    $splitPath = $PropertyPath -split '[\/\.]+'
+
+    $segmented = $PropertyPath.toLower() -replace '[\/\.]+', '.'
+
+    while ($segmented) {
+
+         Write-Verbose "_----------------------------_"
+        Write-Verbose ($Object.GetType().BaseType)
+
+        $objectProperties = $Object.PSObject.Properties
+        if ($Object.GetType().BaseType -eq [System.Array]) {
+            $objectProperties = $Object[0].PSObject.Properties
+        }
+
+        Write-Verbose ($objectProperties.Name | ConvertTo-Json)
+
+        $Target = $objectProperties | `
+         Where-Object { $segmented.Contains($_.Name.toLower()) } | `
+         Sort-Object -Property @{ Expression = { $segmented.IndexOf($_.Name.toLower()) } } | `
+         Select-Object -First 1
+
+        if ($null -eq $Target) {
+            Throw "Path: '$PropertyPath' - Error at '$segmented' - is NULL"
+        }
+
+        Write-Verbose "$segmented, $($Target.name.toLower())"
+        $segmented = $segmented -replace "\.*$($Target.name.toLower())\.*", ''
+         Write-Verbose "$segmented, $($Target.name.toLower())"
+        $Object = $Object."$($Target.name)" # Don't use Target Value, in case $object is Array and multiple need to be returned
+    }
+
+    <#
     foreach ($segment in $splitPath) {
 
         $segment = $segment -replace '[()]+', ''
@@ -33,6 +65,7 @@ function Get-Property {
 
         $Object = $Object."$segment"
     }
+    #>
 
     return $Object
 }

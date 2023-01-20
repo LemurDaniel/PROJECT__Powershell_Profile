0a288199ba614fb65150cd6d409be2e9c0acf468
LemurDaniel <landau.daniel.1998@gmail.com>
2023-01-20 20:13:32 +0100

    92C27C5D

---
+++ Helper.DevOps/AzureDevOps/DataGrabber/Show-DevOpsContext.ps1
@@ -0,0 +1,14 @@
+
+function Show-DevOpsContext {
+
+    [CmdletBinding()]
+    param ()
+
+    $Organization = Get-DevOpsCurrentContext -Organization
+    $Project = Get-DevOpsCurrentContext -Project
+     
+    Write-Host -ForegroundColor GREEN "`nCurrent Context:"
+    Write-Host -ForegroundColor GREEN " 	Organization: $Organization "
+    Write-Host -ForegroundColor GREEN "     Project:      $Project"
+    Write-Host
+}

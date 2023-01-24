6d101472ab6b7ab6c30e6446abbc7dc4406ee86e
LemurDaniel <landau.daniel.1998@gmail.com>
2023-01-24 16:51:29 +0100

    807346FE

--- Helper/Helper.psm1
+++ Helper/Helper.psm1
@@ -1,6 +1,6 @@
 
 
-Import-Module "$PSScriptRoot\..\Helper.Utils" -RequiredVersion 0.0.1 -Global
-Import-Module "$PSScriptRoot\..\Helper.OneDrive" -RequiredVersion 0.0.1 -Global
-Import-Module "$PSScriptRoot\..\Helper.Azure" -RequiredVersion 0.0.1 -Global
-Import-Module "$PSScriptRoot\..\Helper.DevOps" -RequiredVersion 0.0.1 -Global
+Import-Module "$PSScriptRoot\..\Helper.Utils" -MinimumVersion 1.0.1 -Global
+Import-Module "$PSScriptRoot\..\Helper.OneDrive" -MinimumVersion 1.0.1 -Global
+Import-Module "$PSScriptRoot\..\Helper.Azure" -MinimumVersion 1.0.1 -Global
+Import-Module "$PSScriptRoot\..\Helper.DevOps" -MinimumVersion 1.0.1 -Global

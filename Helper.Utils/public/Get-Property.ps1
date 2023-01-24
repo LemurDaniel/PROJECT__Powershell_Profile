9b2f934e4eb99335d644b5639431d8062b3b53a1
LemurDaniel <landau.daniel.1998@gmail.com>
2023-01-24 16:32:28 +0100

    BE0FA773

--- Helper.Utils/public/Get-Property.ps1
+++ Helper.Utils/public/Get-Property.ps1
@@ -46,26 +46,5 @@ function Get-Property {
         $Object = $Object."$($Target.name)" # Don't use Target Value, in case $object is Array and multiple need to be returned
     }
 
-    <#
-    foreach ($segment in $splitPath) {
-
-        $segment = $segment -replace '[()]+', ''
-        Write-Verbose $segment
-        if ($null -eq $Object) {
-            Throw "Path: $PropertyPath - Error at Segment $segment - Object is NULL"
-        }
-
-        #if ($Object.GetType().Name -notin @('PSObject', 'PSCustomObject') ) {
-        #    Throw "Path: $PropertyPath - Error at Segment $segment - Object is $($Object.GetType().Name)"
-        #}
-
-        if ($null -eq $Object."$segment") {
-            Throw "Path: $PropertyPath - Error at Segment $segment - Segment does not exist "
-        }
-
-        $Object = $Object."$segment"
-    }
-    #>
-
     return $Object
 }

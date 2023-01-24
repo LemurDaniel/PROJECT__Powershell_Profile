057d0d4a7707d1bc57c44874f1a47a841faf534c
LemurDaniel <landau.daniel.1998@gmail.com>
2023-01-24 15:53:13 +0000

    (Automated) Generate Meta-Files

---
+++ DevOpsScripts.Utils/DevOpsScripts.Utils.psm1
@@ -0,0 +1,19 @@
+
+        if ($true -and ($PSEdition -eq 'Desktop')) {
+            if ($PSVersionTable.PSVersion -lt [Version]'7.2') {
+                throw 'PowerShell versions lower than 7.2 are not supported. Please upgrade to PowerShell 7.2 or higher.'
+            }
+        }
+
+        # All Files in public will be exported by the Powershell-Module on Import-Module.
+        @(
+            'internal',
+            'public' 
+        ) | `
+            ForEach-Object { Join-Path -Path $PSScriptRoot -ChildPath $_ } | `
+            Get-ChildItem -Recurse -File -Filter '*.ps1' -ErrorAction Stop | `
+            ForEach-Object {
+            . $_.FullName
+        }
+
+    

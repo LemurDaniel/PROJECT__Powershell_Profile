348cb8eeb8449884af6e86b04236edab942a9519
LemurDaniel <Landau.Daniel.1998@gmail.com>
2023-02-09 21:35:52 +0100

    6A108E98

--- DevOpsScripts.Utils/functions/Parser/Start-TFVarsParser.ps1
+++ DevOpsScripts.Utils/functions/Parser/Start-TFVarsParser.ps1
@@ -2,30 +2,77 @@ Using module './classes/AstNodeType.psm1'
 Using module './classes/Parser.psm1'
 
 
-# Not finishied. Very Very basic attempt at a parser.
+# Not finishied. Very Very basic attempt at a parser. This is not supposed to be a Full-Parser, just very basic attempt.
 function Start-TFVarsParser {
     param (
         [Parameter(Mandatory = $false)]
         [System.String]
         $test = @'
-## sdsdcs Comment
-/****
- * 
- * Multiline Comment
- * 
- * */
- ## Comment gets ignored
 
-abc = 2
+# Terraform lower level remote State
+lowerlevel_subscription_id      = "24234342-24-234-234-24"
+lowerlevel_resource_group_name  = "rg-launchpad-dev-001"
+lowerlevel_storage_account_name = "stbrzacfstatedev001"
+lowerlevel_container_name       = "acf-level2-hub"
+lowerlevel_key                  = "landingzone_acf_hub.tfstate"
 
-abc = {
-    property1 = 123
-    # property2 = 123
-    property3 = {
-        property1 = "123"
-        # property2 = 123
-    }
+# Environment Variables
+convention  = "" # Sourced from landingzone_acf_hub
+prefix      = "" # Sourced from landingzone_acf_hub
+environment = "" # Sourced from landingzone_acf_hub
+location    = "westeurope"
+
+# Set of tags for resource groups
+tags = {
+  govAccountable         = "name.surname@brz.eu"
+  govBilling             = "Corporate"
+  govBusinessCriticality = "Medium"
+  govCompany             = "123456789"
+  govCostCenter          = "123456789"
+  govDeploymentType      = "IaC"
+  govResponsible         = "name.surname@brz.eu"
+  govWorkloadDescription = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna"
 }
+
+data_actions = ["sss", "ssss"
+
+, "adasdasd"
+]
+
+testtesttest = [
+    {  
+        Property1 = 123
+        Property2 = "123"
+    }
+]
+
+permissions = {
+  actions      = ["*"]
+  data_actions = []
+  not_actions = [
+    "Microsoft.ManagedIdentity/userAssignedIdentities/assign/action",
+    "Microsoft.ManagedIdentity/userAssignedIdentities/write",
+    "Microsoft.ManagedIdentity/userAssignedIdentities/delete",
+    "Microsoft.Authorization/classicAdministrators/write",
+    "Microsoft.Authorization/elevateAccess/action",
+    "Microsoft.Authorization/locks/write",
+    "Microsoft.Authorization/locks/delete",
+    "Microsoft.Authorization/policyDefinitions/write",
+    "Microsoft.Authorization/policyDefinitions/delete",
+    "Microsoft.Authorization/policyAssignments/write",
+    "Microsoft.Authorization/policyAssignments/delete",
+    "Microsoft.Authorization/policySetDefinitions/write",
+    "Microsoft.Authorization/policySetDefinitions/delete",
+    "Microsoft.Authorization/roleDefinitions/delete",
+    "Microsoft.Authorization/roleDefinitions/write",
+    "Microsoft.Management/managementGroups/subscriptions/delete",
+    "Microsoft.Management/managementGroups/subscriptions/write",
+    "Microsoft.Management/managementGroups/write",
+    "Microsoft.Network/virtualNetworks/peer/action",
+  ]
+  not_data_actions = []
+}
+
 '@
     )
     
@@ -40,6 +87,9 @@ abc = {
         [AstNodeType]::new('BLOCK_END', '^}'),
         [AstNodeType]::new('BLOCK_START', '^{'),
 
+        [AstNodeType]::new('ARRAY_END', '^\]'),
+        [AstNodeType]::new('ARRAY_START', '^\['),
+        [AstNodeType]::new('ARRAY_SEPERATOR', '^,'),
 
         [AstNodeType]::new('STRING', "^`"[^`"]*`"|^'[^']*'"),
         [AstNodeType]::new('NUMBER', '^\d+')
--- DevOpsScripts.Utils/functions/Parser/classes/Parser.psm1
+++ DevOpsScripts.Utils/functions/Parser/classes/Parser.psm1
@@ -11,6 +11,7 @@ class Parser {
         $this.tokenizer = [Tokenizer]::new($Configuration)
     }
 
+    # Intialize parse
     [AstNode] parse($content) {
         $this.tokenizer.set($content)
         $null = $this.tokenizer.advanceNextToken()
@@ -21,15 +22,18 @@ class Parser {
         )
     }
 
-    ##################
+
     [AstNode[]] StatementList() {
         return $this.StatementList($null)
     }
     [AstNode[]] StatementList($stopLookahead) {
+        return $this.StatementList($stopLookahead, 'Statement')
+    }
+    [AstNode[]] StatementList($stopLookahead, $StatementMethod) {
         $statementList = @()
 
         while ($stopLookahead -ne $this.tokenizer.current.Type) {
-            $statement = $this.Statement()
+            $statement = $this.$StatementMethod()
             if ($null -ne $statement) {
                 $statementList += $statement
             }
@@ -38,13 +42,21 @@ class Parser {
         return $statementList
     }
 
-    ##################
+
+    # Expectes either seperators or an Assignment.
     [AstNode] Statement() {
         switch ($this.tokenizer.current.Type) {
             SEPERATOR {
                 $this.eat($this.Seperator.Type)
                 return $null
             }
+        }
+        return $this.AssignmentExpression()
+    }
+
+    # Expect either Blocks, Array or Literals.
+    [AstNode] BasicStatement() {
+        switch ($this.tokenizer.current.Type) {
             BLOCK_START {
                 $this.eat('BLOCK_START')
                 $body = $this.tokenizer.current.Type -eq 'BLOCK_END' ? @() : $this.StatementList('BLOCK_END')
@@ -54,44 +66,45 @@ class Parser {
                     $body
                 )
             }
-        }
-        return $this.ExpressionStatement()
-    }
-
-    [AstNode] ExpressionStatement() {
-
-        switch ($this.tokenizer.current.Type) {
-
-            'VARIABLE' {
-                return $this.AssignmentExpression()
+            ARRAY_START {
+                $this.eat('ARRAY_START')
+                $body = $this.tokenizer.current.Type -eq 'ARRAY_END' ? @() : $this.StatementList('ARRAY_END', 'ArrayExpression')
+                $this.eat('ARRAY_END')
+                return [AstNode]::new(
+                    'Array',
+                    $body
+                )
             }
-
-            Default {}
         }
-
         return $this.Literal()
     }
 
+    # Expectes Elements and ArrayEnd or ArraySeperators
+    [AstNode] ArrayExpression() {
 
+        $element = $this.BasicStatement()
+        if ($this.tokenizer.current.type -ne 'ARRAY_END') {
+           $this.eat('ARRAY_SEPERATOR')
+        }
+        return $element
 
-    ##################
+    }
+
+    # Assignment Expressions expect a Variable identifier and a following BasicStatement
     [AstNode] AssignmentExpression() {
-        $variable = $this.Variable()
+        $variable = $this.eat('VARIABLE')
         $this.eat('ASSIGNMENT')
         return [AstNode]::new(
             'AssignmentExpression',
             @(
                 $variable,
-                $this.Statement()
+                $this.BasicStatement()
             )
         )
     }
 
-    [AstNode] Variable() {
-        return $this.eat('VARIABLE')
-    }
 
-    ##################
+    # Literals expect NumberLiterals or StringLiterals
     [AstNode] Literal() {
 
         switch ($this.tokenizer.current.Type) {
@@ -124,6 +137,7 @@ class Parser {
     }
 
 
+    # Consumes and advances to the next token.
     [AstNode] eat($tokenType) {
         return $this.eat($tokenType, $false)
     }

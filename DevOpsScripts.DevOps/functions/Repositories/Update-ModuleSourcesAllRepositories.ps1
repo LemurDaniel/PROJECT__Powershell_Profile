50c8eacdd298eb3bbb7d2a2d719abb9fc235c42d
LemurDaniel <Landau.Daniel.1998@gmail.com>
2023-02-22 12:04:48 +0100

    2CAA1343

--- DevOpsScripts.DevOps/functions/Repositories/Update-ModuleSourcesAllRepositories.ps1
+++ DevOpsScripts.DevOps/functions/Repositories/Update-ModuleSourcesAllRepositories.ps1
@@ -15,6 +15,11 @@
         
 #>
 function Update-ModuleSourcesAllRepositories {
+
+    [cmdletbinding(
+        SupportsShouldProcess = $true,
+        ConfirmImpact = 'high'
+    )]
     param (
         # Refresh cached values.
         [Parameter()]
@@ -22,34 +27,46 @@ function Update-ModuleSourcesAllRepositories {
         $refresh = $false
     )
 
-    if ((Get-DevOpsContext -Project) -ne 'DC Azure Migration'){
+    if ((Get-DevOpsContext -Project) -ne 'DC Azure Migration') {
         throw 'Set Context to DC Azure Migration'
     }
     
-    
-    # Peform regex on following last
-    $sortOrder = @(
+
+    #Referenced By other Modules
+    $moduleUpdatesRequired = @(
+        'terraform-azurerm-acf-naming',
+        'terraform-azurerm-acf-monitoring'
+    )
+
+    # Require feature branch and fetch Versions again.
+    $EnteredNonModules = $false
+    $nonModules = @(
         'terraform-acf-main',
         'terraform-acf-adds',
         'terraform-acf-launchpad'
     )
-
-    $forceFeatureBranch = @(
-        'terraform-acf-main',
-        'terraform-acf-adds',
-        'terraform-acf-launchpad'
-    )
-
 
 
     $null = Get-RecentSubmoduleTags -refresh:($refresh)
-    $allTerraformRepositories = Get-ProjectInfo -return 'repositories' | `
-        Where-Object -Property name -Like '*terraform*' | `
-        Sort-Object -Property { $sortOrder.IndexOf($_.name) }
+    $allTerraformRepositories = Get-ProjectInfo -return 'repositories' | Where-Object -Property name -Like '*terraform*' 
 
+    $sortedLast = $allTerraformRepositories | Where-Object -Property name -In $nonModules | Sort-Object -Property { $nonModules.IndexOf($_.name) }
+    $sortedFirst = $allTerraformRepositories | Where-Object -Property name -In $moduleUpdatesRequired | Sort-Object -Property { $moduleUpdatesRequired.IndexOf($_.name) }
+    $terraformOther = $allTerraformRepositories | Where-Object -Property name -NotIn $moduleUpdatesRequired | Where-Object -Property name -NotIn $nonModules
 
-    foreach ($repository in $allTerraformRepositories) {
+    $repositoriesInOrder = $sortedFirst + $terraformOther + $sortedLast
+
+    foreach ($repository in $repositoriesInOrder) {
+
+        if (!$EnteredNonModules -AND $repository.Name -in $nonModules) {
+            Write-Host "`n---------------------------------------------------------------`n"
+            Write-Host -ForegroundColor Red '   Fnished Processing all Modules'
+            Write-Host -ForegroundColor Red '   The script will fetch all tags again!'
+            Write-Host "`n---------------------------------------------------------------`n"
+            $null = Get-RecentSubmoduleTags -refresh
+        }
     
+        Write-Host "`n------------------------------`n"
         Write-Host -ForegroundColor Yellow "Searching Respository '$($repository.name)' locally"
         $null = Open-Repository -Name ($repository.name) -onlyDownload
 
@@ -65,30 +82,35 @@ function Update-ModuleSourcesAllRepositories {
 
         Write-Host -ForegroundColor Yellow 'Search and Replace Submodule Source Paths'
         $replacements = Update-ModuleSourcesInPath -replacementPath ($repository.Localpath) -Confirm:$false
-        if ($replacements.Count -eq 0) {
-            continue;
-        }
 
         # Case when feature branch is needed
-        if ($forceFeatureBranch.Contains($repository.name)) {
+        if ($replacements.Count -ne 0 -AND $nonModules.Contains($repository.name)) {
         
-            Write-Host -ForegroundColor Yellow 'Create Feature Branch and create Pull Request'
-            git -C $repository.Localpath checkout -B features/AUTO__Update-Submodule-source-path
-            git -C $repository.Localpath add -A
-            git -C $repository.Localpath commit -m 'AUTO--Update Submodule Source Paths'
-            git -C $repository.Localpath push origin features/AUTO__Update-Submodule-source-path
+            if ($PSCmdlet.ShouldProcess($repository.Name , 'Create Pull Request')) {
+                Write-Host -ForegroundColor Yellow 'Create Feature Branch and create Pull Request'
+                git -C $repository.Localpath checkout -B features/AUTO__Update-Submodule-source-path
+                git -C $repository.Localpath add -A
+                git -C $repository.Localpath commit -m 'AUTO--Update Submodule Source Paths'
+                git -C $repository.Localpath push origin features/AUTO__Update-Submodule-source-path
         
-            if ($repoRefs[0].name.contains('dev')) {
-                New-PullRequest -PRtitle 'DEV - AUTO--Update Submodule Source Paths' -Target 'dev' `
-                    -Id ($repository.id) -Path ($repository.Localpath) #-projectName ($repository.remoteUrl.split('/')[4])
+                if ($repoRefs[0].name.contains('dev')) {
+                    New-PullRequest -PRtitle 'DEV - AUTO--Update Submodule Source Paths' -Target 'dev' `
+                        -Id ($repository.id) -Path ($repository.Localpath) #-projectName ($repository.remoteUrl.split('/')[4])
+                }
+                else {
+                    New-PullRequest -PRtitle 'DEV - AUTO--Update Submodule Source Paths' -Target 'default' `
+                        -Id ($repository.id) -Path ($repository.Localpath) #-projectName ($repository.remoteUrl.split('/')[4])
+                }
             }
             else {
-                New-PullRequest -PRtitle 'DEV - AUTO--Update Submodule Source Paths' -Target 'default' `
-                    -Id ($repository.id) -Path ($repository.Localpath) #-projectName ($repository.remoteUrl.split('/')[4])
+                Write-Host -ForegroundColor Red "`n The Script will exit now! `n"
+                return 
             }
 
         }
-        else {
+
+        # Case when no feature branch is needed
+        if ($replacements.Count -ne 0 -AND !$nonModules.Contains($repository.name)) {
         
             Write-Host -ForegroundColor Yellow 'Update Dev Branch and create Pull Request'
             git -C $repository.Localpath checkout -B dev
@@ -100,5 +122,24 @@ function Update-ModuleSourcesAllRepositories {
                 -Id ($repository.id) -Path ($repository.Localpath) #-projectName ($repository.remoteUrl.split('/')[4])
 
         }
+    
+
+     
+        if ($repository.Name -in $moduleUpdatesRequired) {
+            Write-Host "`n---------------------------------------------------------------`n"
+            Write-Host -ForegroundColor Red "   '$($repository.Name)' is referenced by Successor-Modules."
+            Write-Host -ForegroundColor Red '   Please update the Module before proceeding.'
+            Write-Host -ForegroundColor Red '   The script will fetch all tags again!'
+            Write-Host "`n---------------------------------------------------------------`n"
+
+            if ($PSCmdlet.ShouldProcess('Update Module Tags Cache')) {
+                $null = Get-RecentSubmoduleTags -refresh
+            }
+            else {
+                Write-Host -ForegroundColor Red "`n The Script will exit now! `n"
+                return 
+            }
+        }
+       
     }
 }

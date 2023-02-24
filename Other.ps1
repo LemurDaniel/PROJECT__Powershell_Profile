25875f68dfb8e9ce23281eccb426c6c64877c81b
LemurDaniel <Landau.Daniel.1998@gmail.com>
2023-02-24 14:16:00 +0100

    D9DF11E7

--- Other.ps1
+++ Other.ps1
@@ -2,63 +2,64 @@
 
 $global:DefaultEnvPaths = @{
     
-    WindowsAppsFolder = 'C:\Users\M01947\AppData\Local\Microsoft\WindowsApps' #TODO
+    WindowsAppsFolder   = 'C:\Users\M01947\AppData\Local\Microsoft\WindowsApps' #TODO
 
     # Some default
-    winget            = "$env:HOMEDRIVE\$env:HOMEPATH\AppData\Local\Microsoft\WindowsApps"
-    System32          = 'C:\Windows\system32'
-    wbem              = 'C:\Windows;C:\Windows\System32\Wbem'
-    OpenSSH           = 'C:\Windows\System32\OpenSSH\'
-    ThinPrint         = 'C:\Program Files\ThinPrint Client\'
-    ThinPrintx86      = 'C:\Program Files (x86)\ThinPrint Client\'
+    winget              = "$env:HOMEDRIVE\$env:HOMEPATH\AppData\Local\Microsoft\WindowsApps"
+    System32            = 'C:\Windows\system32'
+    wbem                = 'C:\Windows;C:\Windows\System32\Wbem'
+    OpenSSH             = 'C:\Windows\System32\OpenSSH\'
+    ThinPrint           = 'C:\Program Files\ThinPrint Client\'
+    ThinPrintx86        = 'C:\Program Files (x86)\ThinPrint Client\'
 
     # Code Editors
-    VSCode            = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\Microsoft VS Code\bin" -ErrorAction SilentlyContinue).Path `
+    VSCode              = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\Microsoft VS Code\bin" -ErrorAction SilentlyContinue).Path `
         ?? 'C:\Program Files\Microsoft VS Code\bin'
     #"C:\Users\Daniel\AppData\Local\Programs\Microsoft VS Code\bin"
 
     # Powershell
-    WindowsPowerShell = 'C:\Windows\System32\WindowsPowerShell\v1.0\'
-    PowerShell        = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\PowerShell\7.5" -ErrorAction SilentlyContinue).Path `
+    WindowsPowerShell   = 'C:\Windows\System32\WindowsPowerShell\v1.0\'
+    PowerShell          = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\PowerShell\7.5" -ErrorAction SilentlyContinue).Path `
         ?? 'C:\Program Files\PowerShell\7'
 
 
     # CLI Tools
-    AzureCLI          = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\CLI\Azure\CLI2\wbin" -ErrorAction SilentlyContinue).Path `
+    AzureCLI            = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\CLI\Azure\CLI2\wbin" -ErrorAction SilentlyContinue).Path `
         ?? 'C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin'
-    sqlcmd            = "$env:AppPath\_EnvPath_Apps\CLI\Microsoft SQL Server\sqlcmd"
+    sqlcmd              = "$env:AppPath\_EnvPath_Apps\CLI\Microsoft SQL Server\sqlcmd"
  
-    terraform         = $env:TerraformNewestVersion 
-    terraformDocs     = $env:TerraformDocsNewestVersion
+    terraform           = $env:TerraformNewestVersion 
+    terraformDocs       = $env:TerraformDocsNewestVersion
 
 
     # TODO - Sort for newest Version
-    nodejs            = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\nodejs\18.12.1" -ErrorAction SilentlyContinue).Path `
+    node_modules_global = 'C:\Users\Daniel\AppData\Roaming\npm'
+    nodejs              = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\nodejs\18.12.1" -ErrorAction SilentlyContinue).Path `
         ?? 'C:\Program Files\nodejs'
 
-    gitbin            = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\Git\2.38\bin" -ErrorAction SilentlyContinue).Path 
-    gitcmd            = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\Git\2.38\cmd" -ErrorAction SilentlyContinue).Path `
+    gitbin              = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\Git\2.38\bin" -ErrorAction SilentlyContinue).Path 
+    gitcmd              = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\Git\2.38\cmd" -ErrorAction SilentlyContinue).Path `
         ?? 'C:\Program Files\Git\cmd'
-    git               = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\Git\2.38" -ErrorAction SilentlyContinue).Path `
+    git                 = (Resolve-Path -Path "$env:AppPath\_EnvPath_Apps\Git\2.38" -ErrorAction SilentlyContinue).Path `
         ?? 'C:\Program Files\Git'
 
-    dotnet            = (Get-ChildItem -Path 'C:\Program Files' -Directory -Filter 'dotnet').FullName
-    dotnet_1Drv       = (Get-ChildItem -Path "$env:AppPath\_EnvPath_Apps" -Directory -Filter 'dotnet').FullName
+    dotnet              = (Get-ChildItem -Path 'C:\Program Files' -Directory -Filter 'dotnet').FullName
+    dotnet_1Drv         = (Get-ChildItem -Path "$env:AppPath\_EnvPath_Apps" -Directory -Filter 'dotnet').FullName
 
-    vlang             = (Get-ChildItem -Path "$env:AppPath\_EnvPath_Apps" -Directory -Filter 'v')
-    clang             = 'C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.34.31933\bin\Hostx86\x86'
-    cinlude           = 'C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.34.31933\include'
-    cmake             = "$env:AppPath\_EnvPath_Apps\cmake\3.24\bin"
-    gnumake           = "$env:AppPath\_EnvPath_Apps\GnuWin32\3.8\bin"
+    vlang               = (Get-ChildItem -Path "$env:AppPath\_EnvPath_Apps" -Directory -Filter 'v')
+    clang               = 'C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.34.31933\bin\Hostx86\x86'
+    cinlude             = 'C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.34.31933\include'
+    cmake               = "$env:AppPath\_EnvPath_Apps\cmake\3.24\bin"
+    gnumake             = "$env:AppPath\_EnvPath_Apps\GnuWin32\3.8\bin"
 
-    java              = "$env:AppPath\_EnvPath_Apps\javaSDK\jdk-10.0.2\bin"
+    java                = "$env:AppPath\_EnvPath_Apps\javaSDK\jdk-10.0.2\bin"
 
-    docker            = 'C:\Program Files\Docker\Docker\resources\bin'
+    docker              = 'C:\Program Files\Docker\Docker\resources\bin'
 
 
-    gpg               = (Resolve-Path -Path 'C:\Program Files (x86)\GnuPG\bin' -ErrorAction SilentlyContinue).Path `
+    gpg                 = (Resolve-Path -Path 'C:\Program Files (x86)\GnuPG\bin' -ErrorAction SilentlyContinue).Path `
         ?? 'C:\Users\M01947\AppData\Local\Programs\GnuPG\bin'
-    tflint            = "$env:AppPath\_EnvPath_Apps\tflint\v0.44.0"
+    tflint              = "$env:AppPath\_EnvPath_Apps\tflint\v0.44.0"
 }
 
 $SettingsJsonDefaults = [PSCustomObject]@{

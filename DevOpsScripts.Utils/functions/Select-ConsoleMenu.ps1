c30ce9f8b0109b68f133936be48bd31082b6e79d
LemurDaniel <Landau.Daniel.1998@gmail.com>
2023-03-09 15:00:07 +0100

    4B4EAB03

--- DevOpsScripts.Utils/functions/Select-ConsoleMenu.ps1
+++ DevOpsScripts.Utils/functions/Select-ConsoleMenu.ps1
@@ -57,10 +57,13 @@ Function Select-ConsoleMenu {
         $SelectionOptions = $Options
     }
 
+    $selectionIndexOnPage = 0
+    $maxSelectionsPerPage = 10
+    $countOfPages = [System.Math]::Ceiling($Options.Count / 10)
+    $currentPage = 0
 
     $prefixSelected = ' => '
     $prefixNonSelected = '    '
-    $selectionIndex = 0
 
     try {
 
@@ -69,12 +72,19 @@ Function Select-ConsoleMenu {
             [System.Console]::CursorVisible = $false
             [System.Console]::Clear()
           
-            # Show Selection
-            Write-Host -ForegroundColor Magenta ("**$($description.trim())**" | ConvertFrom-Markdown -AsVT100EncodedString).VT100EncodedString
+            Write-Host -ForegroundColor Magenta ("**$($Description.trim())**" | ConvertFrom-Markdown -AsVT100EncodedString).VT100EncodedString
 
-            $SelectionOptions | ForEach-Object { $index = 0 } {
 
-                if ($index -eq $selectionIndex) {
+            $maxSelectionCurrentPage = $maxSelectionsPerPage
+            $selectionPageOffset = $currentPage * $maxSelectionsPerPage
+            $selectionIndex = [System.Math]::Min($maxSelectionCurrentPag, $selectionIndexOnPage + $selectionPageOffset)
+
+            $SelectionOptions | `
+                Select-Object -Skip $selectionPageOffset | `
+                Select-Object -First $maxSelectionsPerPage | `
+                ForEach-Object { $index = 0 } {
+
+                if ($index -eq $selectionIndexOnPage) {
                     Write-Host "$prefixSelected" -NoNewline
                     Write-Host -BackgroundColor Magenta $_
                 } 
@@ -86,13 +96,20 @@ Function Select-ConsoleMenu {
                 $index++
 
             }
+
+            if ($countOfPages -gt 0) {
+                Write-Host
+                #Write-Host -NoNewline "Page " 
+                Write-Host -NoNewline -BackgroundColor white "$($currentPage+1)/$countOfPages"
+                Write-Host
+            }
     
             # Process and switch key presses
             $keyDownEvent = [System.Console]::ReadKey($true)
             Switch ($keyDownEvent.Key) {
     
                 { $_ -eq [System.ConsoleKey]::Enter } {
-                    return $Options[$selectionIndex]
+                    return $Options[$selectionIndexOnPage + $selectionPageOffset]
                 }
 
                 { $_ -eq [System.ConsoleKey]::Escape } {
@@ -100,11 +117,19 @@ Function Select-ConsoleMenu {
                 }
 
                 { $_ -in @([System.ConsoleKey]::W, [System.ConsoleKey]::UpArrow) } {
-                    $selectionIndex = ($selectionIndex + $SelectionOptions.Length - 1) % $SelectionOptions.Length
+                    $selectionIndexOnPage = ($selectionIndexOnPage + $maxSelectionCurrentPage - 1) % $maxSelectionCurrentPage
                 }
     
                 { $_ -in @([System.ConsoleKey]::S, [System.ConsoleKey]::DownArrow) } {
-                    $selectionIndex = ($selectionIndex + $SelectionOptions.Length + 1) % $SelectionOptions.Length
+                    $selectionIndexOnPage = ($selectionIndexOnPage + $maxSelectionCurrentPage + 1) % $maxSelectionCurrentPage
+                }
+
+                { $_ -in @([System.ConsoleKey]::A, [System.ConsoleKey]::LeftArrow) } {
+                    $currentPage = ($currentPage + $countOfPages - 1) % $countOfPages
+                }
+    
+                { $_ -in @([System.ConsoleKey]::D, [System.ConsoleKey]::RightArrow) } {
+                    $currentPage = ($currentPage + $countOfPages + 1) % $countOfPages
                 }
 
                 default {

d8fdf57a44a06d9027b20419cd297c0472f83aaa
LemurDaniel <Landau.Daniel.1998@gmail.com>
2023-03-20 19:41:01 +0100

    89A67D2F

---
+++ DevOpsScripts.DevOps/functions/Repositories/Invoke-ScriptInAllRepositories.ps1
@@ -0,0 +1,118 @@
+
+<#
+    .SYNOPSIS
+    Invokes a script block accross all repositories in a project an creates Pull Requests on Changes.
+
+    .DESCRIPTION
+    Invokes a script block accross all repositories in a project an creates Pull Requests on Changes.
+
+    .INPUTS
+    None. You cannot pipe objects into the Function.
+
+    .OUTPUTS
+    None.
+
+
+    .LINK
+        
+#>
+
+function Invoke-ScriptInAllRepositories {
+
+    [cmdletbinding(
+        SupportsShouldProcess,
+        ConfirmImpact = 'high'
+    )]
+    param (
+        # Autocomplete list for workitems assigned to the user.
+        [Parameter(
+            Mandatory = $true,
+            Position = 0
+        )]
+        [ArgumentCompleter(
+            {
+                param($cmd, $param, $wordToComplete)
+                $validValues = Search-WorkItemInIteration -SearchTags '*' -Current -Personal -return 'fields.System.Title'  
+
+                $validValues | `
+                    ForEach-Object { $_ } | `
+                    Where-Object { $_.toLower() -like "*$wordToComplete*".toLower() } | `
+                    ForEach-Object { $_.contains(' ') ? "'$_'" : $_ } 
+            }
+        )]
+        [System.String]
+        $workitemTitle,
+
+        # The name of the project, if not set default to the Current-Project-Context.
+        [Parameter(
+            Mandatory = $false
+        )]
+        [ValidateScript(
+            { 
+                [System.String]::IsNullOrEmpty($_) -OR $_ -in (Get-DevOpsProjects).name
+            },
+            ErrorMessage = 'Please specify an correct Name.'
+        )]
+        [ArgumentCompleter(
+            {
+                param($cmd, $param, $wordToComplete)
+                $validValues = (Get-DevOpsProjects).name 
+                
+                $validValues | `
+                    Where-Object { $_.toLower() -like "*$wordToComplete*".toLower() } | `
+                    ForEach-Object { $_.contains(' ') ? "'$_'" : $_ } 
+            }
+        )]
+        [System.String]
+        $Project,
+
+        # The script block to invoke in every repository
+        [Parameter(
+            Mandatory = $true
+        )]
+        [System.Management.Automation.ScriptBlock]
+        $ScriptBlock
+    )
+
+
+    $projectTarget = Get-ProjectInfo -Name $Project
+    $projectTarget.repositories | ForEach-Object {
+    
+        Write-Host
+        Write-Host '---------------------------------------------------------------------------'
+
+        $path = Open-Repository -Project $projectTarget.name -Name $_.name -onlyDownload
+
+        Write-Host
+        Write-Host -ForegroundColor Yellow "Processing '$($_.Name)' in '$($_.project.name)'"
+        Write-Host
+
+        git -C $path.FullName stash
+        git -C $path.FullName checkout master
+        git -C $path.FullName pull origin master
+
+        & $ScriptBlock -Repository $_ -Project $_.project
+
+        $count = git -C $path.FullName status --porcelain | Measure-Object | Select-Object -ExpandProperty Count
+        if ($count -gt 0) {
+    
+            Write-Host -ForegroundColor Yellow "Detected Changes in Repository '$($_.name)' in '$($projectTarget.name)'"
+            if ($PSCmdlet.ShouldProcess($_.Name , 'Create Feature Pull Request?')) {
+          
+                New-BranchFromWorkitem -Project $_.project.name -Name $_.name -workitemTitle $workitemTitle
+                git -C $path.FullName add -A
+                git -C $path.FullName commit -m "AUTO--$workitemTitle"
+                git -C $path.FullName push
+                New-PullRequest -PRtitle "AUTO--$workitemTitle" -Source 'current' -Target 'dev' `
+                    -Project ($_.project.name) -RepositoryName $_.name -autocompletion
+            }
+
+            if ($PSCmdlet.ShouldProcess($_.Name , 'Create Master Pull Request?')) {
+                New-PullRequest -PRtitle "AUTO--$workitemTitle" -Source 'dev' -Target 'default' `
+                    -Project ($_.project.name) -RepositoryName $_.name
+            }
+        }
+
+    }
+}
+
--- DevOpsScripts.DevOps/functions/Repositories/Remove-MovedBlocksAllRepositories.ps1
+++ DevOpsScripts.DevOps/functions/Repositories/Remove-MovedBlocksAllRepositories.ps1
@@ -68,40 +68,10 @@ function Remove-MovedBlocksAllRepositories {
         $Project
     )
 
-    $projectTarget = Get-ProjectInfo -Name $Project
-    $projectTarget.repositories | ForEach-Object {
-    
-        Write-Host
-        Write-Host -ForegroundColor Yellow "Checking '$($_.Name)'"
-        Write-Host
-        $path = Open-Repository -Project $projectTarget.name -Name $_.name -onlyDownload
-
-        git -C $path.FullName stash
-        git -C $path.FullName checkout master
-        git -C $path.FullName pull origin master
-        Remove-MovedBlocks -Path $path.FullName
-
-        $count = git -C $path.FullName status --porcelain | Measure-Object | Select-Object -ExpandProperty Count
-        if ($count -gt 0) {
-    
-            Write-Host -ForegroundColor Yellow "Found Moved Blocks in Repository '$($_.name)' in '$($projectTarget.name)'"
-            if ($PSCmdlet.ShouldProcess($_.Name , 'Create Feature Pull Request?')) {
-
-                New-BranchFromWorkitem -Project $_.project.name -Name $_.name -workitemTitle $workitemTitle
-                git -C $path.FullName add -A
-                git -C $path.FullName commit -m 'AUTO--Remove terraform moved blocks'
-                git -C $path.FullName push
-                New-PullRequest -PRtitle 'AUTO--Remove terraform moved blocks' -Source 'current' -Target 'dev' `
-                    -Project ($_.project.name) -RepositoryName $_.name -autocompletion
-
-                    
-                if ($PSCmdlet.ShouldProcess($_.Name , 'Create additional Pull Request from dev to Master?')) {
-                    New-PullRequest -PRtitle 'AUTO--Remove terraform moved blocks' -Source 'dev' -Target 'default' `
-                        -Project ($_.project.name) -RepositoryName $_.name
-                }
-            }
-        }
-
+    Invoke-ScriptInAllRepositories -Project $Project -workitemTitle $workitemTitle -ScriptBlock {
+        param($Repository, $Project)
+        Remove-MovedBlocks -Path $Repository.Localpath
     }
+
 }
 

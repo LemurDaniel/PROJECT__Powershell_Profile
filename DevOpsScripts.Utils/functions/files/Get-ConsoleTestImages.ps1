f6d73e674537594eac66d61e2d38434a25c5ab50
LemurDaniel <Landau.Daniel.1998@gmail.com>
2023-10-26 13:08:21 +0200

    541048FC

--- DevOpsScripts.Utils/functions/files/Get-ConsoleTestImages.ps1
+++ DevOpsScripts.Utils/functions/files/Get-ConsoleTestImages.ps1
@@ -4,6 +4,6 @@ function Get-ConsoleTestImages {
 
     param ()
 
-    return Get-Content -Path (Join-Path -Path $PSScriptRoot -Childpath "console.testimages.json") | ConvertFrom-Json -AsHashtable
+    return Get-Content -Path "$PSScriptRoot/../.resources/console.testimages.json" | ConvertFrom-Json -AsHashtable
 
 }

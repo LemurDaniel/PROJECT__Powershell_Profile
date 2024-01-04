5c1beb54b59cf479c1e1b399f4436f044f855d35
LemurDaniel <Landau.Daniel.1998@gmail.com>
2024-01-04 19:53:50 +0100

    D28ECC7A

--- DevOpsScripts.Stuff/functions/Unsorted/Get-MsRewards.ps1
+++ DevOpsScripts.Stuff/functions/Unsorted/Get-MsRewards.ps1
@@ -6,19 +6,22 @@ function Get-MsRewards {
         $calls,
 
         [Parameter()]
-        [ValidateSet('Edge', 'Opera', 'Chrome')]
+        [ValidateSet('Edge', 'Opera', 'OperaGX', 'Chrome')]
         $browser = 'Opera'
     )
 
-    $words = Get-Content -Path "$PSScriptRoot\41.284_words.txt"
+    $words = Get-Content -Path "C:\Users\Daniel\git\repos\GITHUB\LemurDaniel\LemurDaniel\PROJECT__Powershell_Profile\DevOpsScripts.Stuff\functions\Unsorted\41.284_words.txt"
+    # $words = Get-Content -Path "$PSScriptRoot\41.284_words.txt"
+    
 
     # Default Installation paths for browsers on windows.
     $applicationPaths = @{
-        Edge   = 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
-        Opera  = "C:\Users\$env:USERNAME\AppData\Local\Programs\Opera GX\launcher.exe"
-        Chrome = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
+        Edge    = 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
+        OperaGX = "C:\Users\$env:USERNAME\AppData\Local\Programs\Opera GX\launcher.exe"
+        Opera   = "C:\Users\$env:USERNAME\AppData\Local\Programs\Opera\launcher.exe"
+        Chrome  = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
     }
-    $baseUrl = 'https://www.bing.com/search?q={0}'
+    $baseUrl = 'https://www.bing.com/search?q={0}&form=QBLH'
 
     $progressBar = @{
         Id       = [DateTime]::Now.Minute
@@ -30,7 +33,7 @@ function Get-MsRewards {
 
         $percentComplete = [System.Math]::Floor($current / $calls * 100)
         Write-Progress @progressBar -PercentComplete $percentComplete -Status "$($current+1)/$calls"
-        Start-Sleep -Milliseconds (Get-Random -Minimum 800 -Maximum 2000)
+        Start-Sleep -Milliseconds (Get-Random -Minimum 10000 -Maximum 12000)
         $word = $words[(Get-Random -Minimum 0 -Maximum $words.Length)]
         $url = [System.String]::Format($baseUrl, $word)
         $null = [system.Diagnostics.Process]::Start($applicationPaths[$browser], $url)

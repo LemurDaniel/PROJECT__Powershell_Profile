3f4e17ddf94d0b5c6022edaa3c3241de33d169a8
LemurDaniel <Landau.Daniel.1998@gmail.com>
2024-01-05 01:42:59 +0100

    F560B302

--- DevOpsScripts.Stuff/functions/Unsorted/Get-MsRewards.ps1
+++ DevOpsScripts.Stuff/functions/Unsorted/Get-MsRewards.ps1
@@ -10,10 +10,8 @@ function Get-MsRewards {
         $browser = 'Opera'
     )
 
-    $words = Get-Content -Path "C:\Users\Daniel\git\repos\GITHUB\LemurDaniel\LemurDaniel\PROJECT__Powershell_Profile\DevOpsScripts.Stuff\functions\Unsorted\41.284_words.txt"
-    # $words = Get-Content -Path "$PSScriptRoot\41.284_words.txt"
+    $words = Get-Content -Path "$PSScriptRoot\41.284_words.txt"
     
-
     # Default Installation paths for browsers on windows.
     $applicationPaths = @{
         Edge    = 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
@@ -21,22 +19,35 @@ function Get-MsRewards {
         Opera   = "C:\Users\$env:USERNAME\AppData\Local\Programs\Opera\launcher.exe"
         Chrome  = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
     }
-    $baseUrl = 'https://www.bing.com/search?q={0}&form=QBLH'
 
+    $baseUrl = 'https://www.bing.com/search?q={0}&form=QBLH'
     $progressBar = @{
-        Id       = [DateTime]::Now.Minute
-        Activity = 'Bing Search'
-        Status   = "0/$Calls"
+        PercentComplete = 0
+        Id              = [DateTime]::Now.Minute
+        Activity        = 'Bing Search'
+        Status          = "0/$Calls"
     }
 
     for ($current = 0; $current -lt $calls; $current++) {
 
-        $percentComplete = [System.Math]::Floor($current / $calls * 100)
-        Write-Progress @progressBar -PercentComplete $percentComplete -Status "$($current+1)/$calls"
-        Start-Sleep -Milliseconds (Get-Random -Minimum 10000 -Maximum 12000)
+        
         $word = $words[(Get-Random -Minimum 0 -Maximum $words.Length)]
         $url = [System.String]::Format($baseUrl, $word)
         $null = [system.Diagnostics.Process]::Start($applicationPaths[$browser], $url)
+
+        if ($calls - $current -GT 1) {
+            $progressBar.PercentComplete = [System.Math]::Floor($current / $calls * 100)
+            $sleepMilliseconds = Get-Random -Minimum 10000 -Maximum 12000
+            for ($sleep = 0; $sleep -LT $sleepMilliseconds; $sleep += 100) {
+                Start-Sleep -Milliseconds 100
+
+                $secondsLeft = [System.Math]::Floor(($sleepMilliseconds - $sleep) / 1000)
+                $milliSecondsLeft = [System.Math]::Floor(($sleepMilliseconds - $sleep) % 1000 / 100)
+                $progressBar.Status = "$($current+1)/$calls - (Next in $secondsLeft.$milliSecondsLeft Seconds)"
+
+                Write-Progress @progressBar
+            }
+        }
         
     }
 
